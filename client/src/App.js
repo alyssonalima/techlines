7d084aa1a1acfe5b9aef6dbf969a0ef18789e91d
alyssonalima <alysson-alima@hotmail.com>
2024-11-08 00:29:21 -0300

    start of implementing light and dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,26 @@ import { Provider } from "./components/ui/provider";
 import ProductsScreen from './screens/ProductsScreen';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from "./components/Header";
+import LandingScreen from "./screens/LandingScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+import ThemeProvider from "./components/ThemeProvider";
 
 function App() {
   return (
     <Provider>
-      <Router>
-        <Header />
-        <main>
-          <Routes>
-            <Route path='/' element={<ProductsScreen />} />
-          </Routes>
-        </main>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <Header />
+          <main>
+            <Routes>
+              <Route path='/products' element={<ProductsScreen />} />
+              <Route path='/' element={<LandingScreen />} />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </main>
+        </Router>
+      </ThemeProvider> 
     </Provider>
   )
 }
---
+++ client/src/screens/LandingScreen.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Box, Flex, Heading, HStack, Icon, Image, Link, Skeleton, Stack, Text } from '@chakra-ui/react';
+import { useTheme } from 'next-themes';
+import { FaArrowRight } from 'react-icons/fa';
+import { Link as ReactLink } from 'react-router-dom';
+import { BsPhoneFlip } from 'react-icons/bs';
+
+import ThemeSwitcher from '../components/ThemeSwitcher';
+
+
+const LandingScreen = () => {
+    const { theme } = useTheme();
+
+    return (
+        
+        <Box maxW='8x1' mx='auto' p={{base: '0', lg: '12'}} minH='6x1'>
+            <Stack direction={{base: 'column-reverse', lg: 'row'}} spacing={{base: '0', lg: '20'}}>
+                <Box 
+                    width={{ lg: 'sm' }} 
+                    transform={{ base: 'translateY(-50%)', lg: 'none' }} 
+                    bg={{ base: theme === 'dark' ? 'black' : 'white', lg: 'transparent' }} 
+                    mx={{ base: '6', md: '8', lg: '0' }}
+                    px={{ base: '6', md: '8', lg: '0' }}
+                    py={{ base: '6', md: '8', lg: '12' }}>
+                    <Stack spacing={{ base: '8', lg: '10' }}>
+                        <Stack spacing={{ base: '2', lg: '4' }}>
+                            <ThemeSwitcher />
+                            <Flex alignItems='center'>
+                                <Icon h='12' w='12' color={theme === 'dark' ? 'yellow.200' : 'cyan.500'}>
+                                    <BsPhoneFlip />
+                                </Icon>
+                                <Text fontSize='4xl' fontWeight='bold'>
+                                    Tech Lines
+                                </Text>
+                            </Flex>
+                            <Heading size='xl' fontWeight='normal'>
+                                Refresh your equipment
+                            </Heading>
+                            <HStack spacing='3'>
+                                <Link as={ReactLink} to='/products' color={theme === 'dark' ? 'yellow.200' : 'cyan.500'}>
+                                    Discover now
+                                </Link>
+                                <Icon color={theme === 'dark' ? 'yellow.200' : 'cyan.500'}>
+                                    <FaArrowRight />
+                                </Icon>
+                            </HStack>
+                        </Stack>
+                    </Stack>
+                </Box>
+                <Flex flex='1' overflow='hidden'>
+                    <Image src={theme === 'dark' ? 'images/landing-dark.jpg' : 'images/landing-light.jpg'} fallBack={<Skeleton />} maxH='550px' minH='300px' objectFit='cover' flex='1'/>
+                </Flex>
+            </Stack>
+        </Box>
+    );
+}
+
+export default LandingScreen
